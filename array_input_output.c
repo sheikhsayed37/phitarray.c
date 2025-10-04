daab8ecf8d457de9258cd4e3a541fda7d66e63f1
sheikhsayed37 <sheikhmohammedsayedali@gmail.com>
2025-10-04 19:35:48 +0600

    first commit

---
+++ array_input_output.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a[50];
+    for(int i=0;i<50;i++)
+    {
+        printf("a[%d]\n",i);
+    }
+}
---
+++ arraya_with_loop.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int a[5];
+    int i;
+    for(i=0;i<5;i++)
+    {
+        printf("a[%d]\n",i);
+    }
+}
